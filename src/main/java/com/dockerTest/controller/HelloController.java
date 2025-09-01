fbbd842fb303f8d3a48250ac15e98c632a34a756
Hamid64Raza <hamidraza.raza@gamil.com>
2025-09-01 22:40:16 +0530

    1st commmit

---
+++ src/main/java/com/dockerTest/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.dockerTest.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot Docker on AWS EC2!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running successfully!";
+    }
+}
+
